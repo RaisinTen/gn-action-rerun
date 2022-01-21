976f59cc946006efd14444341396afa7c1976ae7
Darshan Sen <raisinten@gmail.com>
2022-01-21 21:01:39 +0530

    Add () for python print call
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- myscript.py
+++ myscript.py
@@ -3,7 +3,7 @@ import sys
 
 filename = sys.argv[1]
 
-print 'Writing to ' + filename
+print('Writing to ' + filename)
 
 with open(filename, 'w') as out:
     out.write(str(datetime.datetime.now()) + '\n')
